27faff3e213e40d6b582f5d99374598582a2b77e
MassimoCibeiraOcchiuzzi <mcibeiraocchiuzzi@frba.utn.edu.ar>
2026-04-09 20:14:42 -0300

    Resolucion de TP0, codigo de c y modificacion del readme

---
+++ 00-PrimerProgramaC.c
@@ -0,0 +1,10 @@
+// TP0
+
+// Primer programa en c
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hola mundo\n");
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SSL_CibeiraOcchiuzzi_MassimoAndres
+Este es mi repositorio para la cursada de Sintaxis y Semantica de Lenguajes
